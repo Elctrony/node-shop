8487db72903640793ddc515763d0e2f5bd099210
Elctrony <18064@stemegypt.edu.eg>
2022-02-14 17:22:00 +0200

    Revert "adding Model"
    This reverts commit 580a4fb93f4eafb4b013b2394334f52d353f34f1.

--- controller/products.js
+++ controller/products.js
@@ -1,20 +1,22 @@
-const Products = require('../model/products');
-
+const products = [];
 
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
 }
 
 exports.postAddProduct = (req, res, next) => {
-    const title = req.body.title;
-    const product = new Products(title);
-    product.save();
+    const titleData = req.body.title;
+    products.push({ title: titleData });
     res.redirect('/');
 }
 
 exports.getAllProduct = (req, res, next) => {
-    Products.fetchAllProduct((products)=>{
-        res.render('shop', { pageTitle: 'Shop', prods: products, path: '/' })
-    })
+    for (let product of products) {
+        console.log(product)
+    }
+    res.render('shop', { pageTitle: 'Shop', prods: products, path: '/' })
 }
 
+exports.pageNotFound = (req,res,next)=>{
+    res.status(404).render('404',{pageTitle: 'Page not Found'})
+}
